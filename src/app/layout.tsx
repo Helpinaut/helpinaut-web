fe6ba509d03f5f62ef677dd5a8f9ee2647d8854a
Miguel Fernandez <miguelferlez@gmail.com>
2026-03-24 13:23:58 +0100

    feat: add redux store provider in main layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,21 @@
 import type { Metadata } from "next";
-import "@/ui/globals.css";
+import "./globals.css";
+import { ReactNode } from "react";
+import { AppProviders } from "./provider";
 
 export const metadata: Metadata = {
   title: "Helpinaut",
-  description: "A community-driven local services marketplace",
+  description:
+    "Exploring better ways to help with a local services marketplace.",
+  icons: "/helpinaut.svg",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <AppProviders>{children}</AppProviders>
+      </body>
     </html>
   );
 }
---
+++ src/app/provider.tsx
@@ -0,0 +1,7 @@
+import { store } from "@/store/store";
+import { ReactNode } from "react";
+import { Provider } from "react-redux";
+
+export function AppProviders({ children }: { children: ReactNode }) {
+  return <Provider store={store}>{children}</Provider>;
+}
