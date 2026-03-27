f3fc25ca73e016ef639635791fe99ab22e0a5f2c
Miguel Fernandez <miguelferlez@gmail.com>
2026-03-27 11:42:43 +0100

    fix: correct export client component

--- src/app/provider.tsx
+++ src/app/provider.tsx
@@ -4,6 +4,6 @@ import { store } from "@/store/store";
 import { ReactNode } from "react";
 import { Provider } from "react-redux";
 
-export function AppProviders({ children }: { children: ReactNode }) {
+export function AppProvider({ children }: { children: ReactNode }) {
   return <Provider store={store}>{children}</Provider>;
 }
