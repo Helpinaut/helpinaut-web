e15ba30ee44802da099d0615d10bd781bb63f1f4
Miguel Fernandez <miguelferlez@gmail.com>
2026-04-01 14:25:30 +0200

    feat: update favicon based on color scheme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,18 +6,33 @@ import { ThemeProvider } from "@/components/theme-provider";
 import { Geist } from "next/font/google";
 import { cn } from "@/lib/utils";
 
-const geist = Geist({subsets:['latin'],variable:'--font-sans'});
+const geist = Geist({ subsets: ["latin"], variable: "--font-sans" });
 
 export const metadata: Metadata = {
   title: "Helpinaut",
   description:
     "Exploring better ways to help with a local services marketplace.",
-  icons: "/helpinaut.svg",
+  icons: {
+    icon: [
+      {
+        url: "/helpinaut-light.svg",
+        media: "(prefers-color-scheme: dark",
+      },
+      {
+        url: "/helpinaut-dark.svg",
+        media: "(prefers-color-scheme: light",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en" suppressHydrationWarning className={cn("font-sans", geist.variable)}>
+    <html
+      lang="en"
+      suppressHydrationWarning
+      className={cn("font-sans", geist.variable)}
+    >
       <body>
         <AppProvider>
           <ThemeProvider
