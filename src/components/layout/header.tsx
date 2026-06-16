c400667dd862efa62e5fbc13b06f519d57a0a5e9
Miguel Fernandez <miguelferlez@gmail.com>
2026-06-16 12:53:31 +0200

    style: reworked header style

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,168 +1,134 @@
 "use client";
 
-import {
-  ArrowDown,
-  HandCoins,
-  HandPlatter,
-  Plus,
-  Search,
-  User2,
-} from "lucide-react";
-import { Logo } from "../assets/logo";
-import { Input } from "../ui/input";
-import { Button, buttonVariants } from "../ui/button";
-import { Label } from "../ui/label";
-import { useState } from "react";
-import { Separator } from "../ui/separator";
+import { Link } from "@/i18n/navigation";
 import { cn } from "@/lib/utils";
+import { useAppSelector } from "@/store/hooks";
+import { Hand, Plus, Toolbox, User2 } from "lucide-react";
+import { useTranslations } from "next-intl";
+import { Baloo_Da_2 } from "next/font/google";
+import { LanguageSelector } from "../shared/language-selector";
 import { NavigationSheet } from "../shared/navigation-sheet";
+import { ThemeSelector } from "../shared/theme-selector";
+import { Button, buttonVariants } from "../ui/button";
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "../ui/dropdown-menu";
-import { Link } from "@/i18n/navigation";
-import { useAppSelector } from "@/store/hooks";
-import { ThemeSelector } from "../shared/theme-selector";
-import { LanguageSelector } from "../shared/language-selector";
-import { useTranslations } from "next-intl";
 
-export function Header() {
-  const [isMobileSearchOpen, setIsMobileSearchOpen] = useState<boolean>(false);
-  const user = useAppSelector((s) => s.user.user);
+const balooDa2 = Baloo_Da_2({ subsets: ["latin"] });
+
+export default function Header() {
   const t = useTranslations("Header");
-
-  const toggleMobileSearchOpen = () => {
-    setIsMobileSearchOpen((prev) => !prev);
-  };
+  const user = useAppSelector((s) => s.user.user);
 
   return (
-    <header className="bg-card supports-backdrop-filter:bg-card/60 sticky top-0 z-50 w-full border-b backdrop-blur">
+    <>
       <a
-        className="bg-primary text-primary-foreground sr-only flex items-center justify-between gap-1.5 rounded-md px-3! py-1! text-sm font-medium focus:not-sr-only focus:absolute focus:top-4.5 focus:left-7"
-        href="#main"
+        className="bg-primary text-primary-foreground absolute left-0 z-99 m-3 -translate-y-16 rounded-lg px-2.5 py-2 transition focus:translate-y-0"
+        href="#main-content"
       >
         {t("skip-to-content")}
-        <ArrowDown aria-hidden="true" size={16} />
       </a>
-      <nav aria-label="Primary">
-        <div className="container mx-auto px-4 py-4">
-          <div className="flex items-center justify-between gap-4">
-            <div className="flex items-center justify-center gap-2">
-              <div className="item bg-accent-foreground flex h-8 w-8 items-center justify-center rounded-lg">
-                <Logo aria-hidden="true" className="text-card" size={22} />
-              </div>
-              <span className="text-lg font-semibold">Helpinaut</span>
-            </div>
-            <div className="relative hidden max-w-md flex-1 md:flex">
-              <Search
-                aria-hidden="true"
-                className="text-muted-foreground/80 absolute top-1/2 left-3 h-4 w-4 -translate-y-1/2"
-                size={16}
-              />
-              <Label className="sr-only" htmlFor="desktop-search">
-                {t("search.label")}
-              </Label>
-              <Input
-                id="desktop-search"
-                className="peer ps-9"
-                placeholder={t("search.placeholder")}
-                type="search"
-              />
-            </div>
-            <div className="flex items-center gap-2">
-              <Button
-                aria-controls="mobile-search-bar"
-                aria-expanded={isMobileSearchOpen}
-                aria-label={
-                  isMobileSearchOpen
-                    ? t("search.aria-hide")
-                    : t("search.aria-show")
-                }
-                className="md:hidden"
-                onClick={toggleMobileSearchOpen}
-                size="icon"
-                variant="ghost"
+      <header className="fixed inset-x-0 top-5 z-50 px-4">
+        <div className="mx-auto max-w-7xl">
+          <div className="bg-card flex h-16 items-center justify-between rounded-lg border px-6 shadow-lg md:grid md:grid-cols-[1fr_auto_1fr]">
+            <NavigationSheet user={user} />
+            <nav className="hidden items-center gap-2 text-sm lg:flex">
+              <Link
+                className={cn(buttonVariants({ variant: "ghost" }))}
+                href="#how-it-works"
               >
-                <Search aria-hidden="true" />
-              </Button>
+                {t("how-it-works")}
+              </Link>
+              <Link
+                className={cn(buttonVariants({ variant: "ghost" }))}
+                href="/adverts"
+              >
+                {t("browse")}
+              </Link>
+              <Link
+                className={cn(buttonVariants({ variant: "ghost" }))}
+                href="/adverts?isOffer=false"
+              >
+                {t("requests")}
+              </Link>
+              <Link
+                className={cn(buttonVariants({ variant: "ghost" }))}
+                href="/adverts?isOffer=true"
+              >
+                {t("offers")}
+              </Link>
+            </nav>
+            <Link
+              href="/"
+              className="flex items-center justify-self-start sm:justify-self-center"
+            >
+              <span
+                className={cn(
+                  balooDa2.className,
+                  "text-2xl font-bold [text-box-edge:cap_alphabetic] [text-box-trim:trim-both]",
+                )}
+              >
+                Helpinaut
+              </span>
+            </Link>
+            <div className="flex items-center gap-2 md:justify-self-end">
+              <ThemeSelector className="xs:flex hidden" />
+              <LanguageSelector className="xs:flex hidden" />
+              <Link
+                className={cn(buttonVariants({ variant: "ghost" }))}
+                href={user ? "/profile" : "/login"}
+                aria-label={user ? t("user.profile") : t("user.login")}
+              >
+                <User2 className="flex" />
+              </Link>
               <DropdownMenu>
                 <DropdownMenuTrigger
                   aria-label={t("create.label")}
                   render={
-                    <Button className="size-8 sm:h-8 sm:w-auto">
-                      <Plus aria-hidden="true" />
-                      <span className="hidden sm:inline">
+                    <Button
+                      variant="default"
+                      size="icon"
+                      className="lg:w-auto lg:px-2.5"
+                    >
+                      <Plus className="flex lg:hidden" />
+                      <span className="hidden lg:flex">
                         {t("create.label")}
                       </span>
                     </Button>
                   }
-                  type="button"
                 />
                 <DropdownMenuContent className="w-full space-y-1">
                   <DropdownMenuItem
                     render={
-                      <Link href="#" className="flex items-center gap-2">
-                        <HandCoins aria-hidden="true" />
+                      <Link
+                        href="#"
+                        className="flex cursor-pointer items-center gap-2"
+                      >
+                        <Hand />
                         {t("create.request-help")}
                       </Link>
                     }
                   />
                   <DropdownMenuItem
                     render={
-                      <Link href="#" className="flex items-center gap-2">
-                        <HandPlatter aria-hidden="true" />
+                      <Link
+                        href="#"
+                        className="flex cursor-pointer items-center gap-2"
+                      >
+                        <Toolbox />
                         {t("create.offer-help")}
                       </Link>
                     }
                   />
                 </DropdownMenuContent>
               </DropdownMenu>
-              <Link
-                href="/login"
-                className={cn(
-                  buttonVariants({ variant: "default" }),
-                  "hidden size-8 h-8 md:flex lg:w-auto",
-                )}
-              >
-                <User2 aria-hidden="true" />
-                <span className="hidden lg:inline">{t("user.label")}</span>
-              </Link>
-              <LanguageSelector className="hidden md:flex" />
-              <ThemeSelector className="hidden md:flex" />
-              <NavigationSheet user={user} />
-            </div>
-          </div>
-          <div
-            id="mobile-search-bar"
-            className={cn(
-              "overflow-hidden transition-all duration-300 md:hidden",
-              isMobileSearchOpen
-                ? "max-h-40 translate-y-0 opacity-100"
-                : "max-h-0 -translate-y-2 opacity-0",
-            )}
-          >
-            <Separator className="my-4" />
-            <div className="relative">
-              <Search
-                aria-hidden="true"
-                className="text-muted-foreground/80 absolute top-1/2 left-3 -translate-y-1/2"
-                size={16}
-              />
-              <Label className="sr-only" htmlFor="mobile-search">
-                {t("search.label")}
-              </Label>
-              <Input
-                id="mobile-search"
-                className="peer ps-9 text-sm"
-                placeholder={t("search.placeholder")}
-                type="search"
-              />
             </div>
           </div>
         </div>
-      </nav>
-    </header>
+      </header>
+    </>
   );
 }
